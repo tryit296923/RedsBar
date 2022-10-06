21dd32260d4677f43b85fe9ae7b492978e29cb35
mao0211 <maoliao0211@gmail.com>
2022-10-07 05:03:10 +0800

    1.booking controller

---
+++ Alcoholic/Controllers/BookingController.cs
@@ -0,0 +1,29 @@
+﻿using Alcoholic.Models.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Alcoholic.Controllers
+{
+    public class BookingController : Controller
+    {
+        public BookingController()
+        {
+
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Check(string datepicker,int people)
+        {
+            ViewBag.Datepicker = datepicker;
+            ViewBag.People = people;
+            return View();
+        }
+        public IActionResult Success(BookingSuccessViewModels data)
+        {
+            
+            return View(data);
+        }
+
+    }
+}
---
+++ Alcoholic/Models/ViewModels/BookingSuccessViewModels.cs
@@ -0,0 +1,11 @@
+﻿namespace Alcoholic.Models.ViewModels
+{
+    public class BookingSuccessViewModels
+    {
+        public string DatePicker { get; set; }
+        public string People { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
