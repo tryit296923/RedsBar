59f7520ec2593de8f7378e35aff376b276dfb84f
marzlcc <cyh87321@gmail.com>
2022-10-14 16:56:28 +0800

    1. add SendProductsModel 2. remove OrderViewModel 3. edit OrderTemplate, _LayoutMobile 4. edit view( Cart, Check, Success, Total) 5. edit OrderController, ProductController

--- Alcoholic/Controllers/ProductController.cs
+++ Alcoholic/Controllers/ProductController.cs
@@ -1,8 +1,10 @@
 ﻿using Alcoholic.Models.DTO;
 using Alcoholic.Models.Entities;
+using Alcoholic.Models.ViewModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using System.Security.Policy;
+using Newtonsoft.Json;
+using System.Security.Claims;
 
 namespace Alcoholic.Controllers
 {
@@ -22,9 +24,25 @@ namespace Alcoholic.Controllers
         }
 
         [HttpPost]
-        public bool AddToCart(Product product)
+        public string AddToCart([FromBody]SendProductsModel model)
         {
-            return true;
+            CookieOptions cookieOptions = new CookieOptions();
+            cookieOptions.Expires = new DateTimeOffset(DateTime.Now.AddHours(2));
+            string jsonStr = JsonConvert.SerializeObject(model);
+            HttpContext.Response.Cookies.Append("memberCart", jsonStr, cookieOptions);
+            //var memberId = User.Claims.FirstOrDefault(x => x.Type == ClaimTypes.Name).Value;
+            //if(memberId == null)
+            //{
+            //    return NotFound();
+            //}
+            //var member = projectContext.Members.Include("Order").FirstOrDefault(x => x.MemberID == memberId);
+            //if( member == null)
+            //{
+            //    return NotFound();
+            //}
+
+            return "HI";
+            
         }
     }
 }
