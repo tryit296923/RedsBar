58827c4e834d43c90297fe3d61acbb2fa8b3d573
mao0211 <maoliao0211@gmail.com>
2022-10-19 21:26:52 +0800

    1.date format not repaired 2.add booking viewmodel

--- Alcoholic/Controllers/BookingController.cs
+++ Alcoholic/Controllers/BookingController.cs
@@ -1,41 +1,51 @@
-﻿using Alcoholic.Models.Entities;
+﻿using Alcoholic.Models.DTO;
+using Alcoholic.Models.Entities;
 using Alcoholic.Models.ViewModels;
 using Alcoholic.Services;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 
 namespace Alcoholic.Controllers
 {
     public class BookingController : Controller
     {
-        private readonly MailService mailService;
+        private readonly MailService mail;
         private readonly db_a8de26_projectContext _db;
 
         public BookingController(MailService mailService,db_a8de26_projectContext projectContext)
         {
-            this.mailService = mailService;
+            this.mail = mailService;
             this._db = projectContext;
         }
         public IActionResult Booking()
         {
             return View();
         }
-        public IActionResult Check(string ReserveDate, int Number)
+        public IActionResult Check(BookingCheckModel bookingData)
         {
-            ViewBag.ReserveDate = ReserveDate;
-            ViewBag.Number = Number;
-            return View();
+            var model = new BookingCheckModel() {
+                ReserveDate = bookingData.ReserveDate,
+                Number = bookingData.Number,
+            };
+            return View(model);
         }
-        public async Task<IActionResult> Success(Reserves bookingData)
+        public async Task<IActionResult> Success(BookingModel bookingData)
         {
-            var task = Razor.Templating.Core.RazorTemplateEngine.RenderAsync<Reserves>("Views/EmailTemplate/BookingTemplate.cshtml", bookingData);
-            bookingData.ReserveSet = DateTime.Now;
-            _db.Add(bookingData);
+            Reserves reserves = new Reserves();
+            var task = Razor.Templating.Core.RazorTemplateEngine.RenderAsync<BookingModel>
+                ("Views/EmailTemplate/BookingTemplate.cshtml", bookingData);
+            reserves.ReserveDate = bookingData.ReserveDate;
+            reserves.ReserveName = bookingData.ReserveName;
+            reserves.Phone = bookingData.Phone;
+            reserves.Email = bookingData.Email;
+            reserves.Number = bookingData.Number;
+            reserves.ReserveSet = DateTime.Now;
+            
+            _db.Add(reserves);
             _db.SaveChanges();
             var result = await task;
-            mailService.SendMail(bookingData.Email, result, "Reds Bar感謝您的訂位");
+            mail.SendMail(bookingData.Email, result, "RedsBar感謝您的訂位");
 
-            return View(bookingData);
+            return View(reserves);
         }
     }
 }
---
+++ Alcoholic/Models/DTO/BookingModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Alcoholic.Models.DTO
+{
+    public class BookingModel
+    {
+        public DateTime ReserveDate { get; set; }
+        public int Number { get; set; }
+        public string ReserveName { get; set; }
+        public string Phone { get; set; }
+        public string Email { get; set; }
+    }
+}
