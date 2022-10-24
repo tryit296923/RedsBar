1c7eb03031f467c927193f1655be9957228f77b2
tryit296923 <tryit296923@gmail.com>
2022-10-25 01:11:29 +0800

    1. new migarion new pk in orderdetail

---
+++ Alcoholic/Migrations/20221024171043_newpkinorderdetail.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alcoholic.Migrations
+{
+    public partial class newpkinorderdetail : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails",
+                columns: new[] { "OrderId", "Sequence", "ProductId" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_OrderDetails",
+                table: "OrderDetails",
+                columns: new[] { "OrderId", "Sequence" });
+        }
+    }
+}
