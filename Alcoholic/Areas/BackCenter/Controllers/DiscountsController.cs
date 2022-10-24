b87a225b1d2700897952a478166babb19bed5666
Vincent <tyrionv9527@gmail.com>
2022-10-24 16:12:57 +0800

    Edit backcenter - split to 4 sections: 1.add discounts' controller & page 2.add datacenter's page 3.edit backcenter page & rename action's name from backindex to index

---
+++ Alcoholic/Areas/BackCenter/Controllers/DiscountsController.cs
@@ -0,0 +1,161 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Alcoholic.Models.Entities;
+
+namespace Alcoholic.Areas.BackCenter.Controllers
+{
+    [Area("BackCenter")]
+    public class DiscountsController : Controller
+    {
+        private readonly db_a8de26_projectContext _context;
+
+        public DiscountsController(db_a8de26_projectContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BackCenter/Discounts
+        public async Task<IActionResult> Index()
+        {
+              return View(await _context.Discount.ToListAsync());
+        }
+
+        // GET: BackCenter/Discounts/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.Discount == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discount
+                .FirstOrDefaultAsync(m => m.DiscountId == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // GET: BackCenter/Discounts/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BackCenter/Discounts/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("DiscountId,DiscountName,DiscountAmount")] Discount discount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(discount);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        // GET: BackCenter/Discounts/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.Discount == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discount.FindAsync(id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+            return View(discount);
+        }
+
+        // POST: BackCenter/Discounts/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("DiscountId,DiscountName,DiscountAmount")] Discount discount)
+        {
+            if (id != discount.DiscountId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(discount);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!DiscountExists(discount.DiscountId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(discount);
+        }
+
+        // GET: BackCenter/Discounts/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.Discount == null)
+            {
+                return NotFound();
+            }
+
+            var discount = await _context.Discount
+                .FirstOrDefaultAsync(m => m.DiscountId == id);
+            if (discount == null)
+            {
+                return NotFound();
+            }
+
+            return View(discount);
+        }
+
+        // POST: BackCenter/Discounts/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.Discount == null)
+            {
+                return Problem("Entity set 'db_a8de26_projectContext.Discount'  is null.");
+            }
+            var discount = await _context.Discount.FindAsync(id);
+            if (discount != null)
+            {
+                _context.Discount.Remove(discount);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool DiscountExists(int id)
+        {
+          return _context.Discount.Any(e => e.DiscountId == id);
+        }
+    }
+}
