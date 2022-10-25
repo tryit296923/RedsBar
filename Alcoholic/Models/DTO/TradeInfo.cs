c3d742edd02b16ebf91863189e2d8b11163fb728
marzlcc <cyh87321@gmail.com>
2022-10-25 23:01:33 +0800

    1. create AesService (undone) 2. edit TradeInfo (Model) 3. edit OrderApiController (add tradeInfo, undone) 4. edit ordering pages (Check, Total) 5. edit OrderController

--- Alcoholic/Models/DTO/TradeInfo.cs
+++ Alcoholic/Models/DTO/TradeInfo.cs
@@ -4,10 +4,10 @@
     {
         public string MerchantID { get; internal set; }
         public string RespondType { get; internal set; }
-        public long TimeStamp { get; internal set; }
+        public string TimeStamp { get; internal set; }
         public string Version { get; internal set; }
         public string MerchantOrderNo { get; internal set; }
-        public object Amt { get; internal set; }
+        public string Amt { get; internal set; }
         public string ItemDesc { get; internal set; }
         public string? ReturnURL { get; internal set; }
     }
