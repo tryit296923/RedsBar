f193bf0a746fb7efa518ea120697ded320a0e823
tryit296923 <tryit296923@gmail.com>
2022-10-26 17:40:58 +0800

    backcenter login event new controller for staff

--- Alcoholic/Areas/BackCenter/Controllers/MemberController.cs
+++ Alcoholic/Areas/BackCenter/Controllers/MemberController.cs
@@ -26,29 +26,19 @@ namespace Alcoholic.Areas.BackCenter.Controllers
         {
             return View();
         }
-        public IActionResult Staff()
-        {
-            return View();
-        }
         public IActionResult LoginPage()
         {
             return View();
         }
-        public ActionResult ResendAuthPage()
+        public IActionResult ResendAuthPage()
         {
             return View();
         }
         
 
-
-        public IActionResult GetMember(Member memberData)
+        public List<Member> GetAllMember()
         {
-            Member? member = db.Members.Find(memberData);
-            if (member == null)
-            {
-                return NotFound();
-            }
-            return Ok(member);
+            return db.Members.ToList();
         }
 
         public IActionResult AuthByStaff(string EmailID)
@@ -69,52 +59,5 @@ namespace Alcoholic.Areas.BackCenter.Controllers
             db.SaveChanges();
             return Ok(true);
         }
-
-
-
-        [HttpPost]
-        public IActionResult Login([FromBody] Employee emp)
-        {
-            Employee? employee = (from em in db.Employees
-            where emp.EmpAccount == em.EmpAccount
-                                  select em).SingleOrDefault();
-            if (employee == null)
-            {
-                return NotFound();
-            }
-            if (hash.GetHash(emp.EmpPassword) == employee.EmpPassword)
-            {
-                List<Claim> claims = new()
-                {
-                    new Claim(ClaimTypes.Name,emp.EmpName),
-                    new Claim(ClaimTypes.Role,emp.Role),
-                };
-                ClaimsIdentity claimsIdentity = new(claims, CookieAuthenticationDefaults.AuthenticationScheme);
-                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
-                return Ok();
-            }
-            return NotFound();
-        }
-        [Authorize(Roles = "moderater,leader,staff")]
-        public IActionResult LogOut()
-        {
-            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
-            return Ok();
-        }
-        [Authorize(Roles = "moderater")]
-        [HttpPost]
-        public IActionResult Register([FromBody] Employee emp)
-        {
-            if (emp == null)
-            {
-                emp.Salt = Guid.NewGuid().ToString("N");
-                emp.EmpPassword = hash.GetHash((string)emp.EmpPassword.Concat(emp.Salt));
-                db.Add(emp);
-                db.SaveChanges();
-                return Ok();
-            }
-            return NotFound();
-        }
-
     }
 }
---
+++ Alcoholic/Areas/BackCenter/Controllers/StaffController.cs
@@ -0,0 +1,76 @@
+﻿using Alcoholic.Models.Entities;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
+using System.Security.Claims;
+using Alcoholic.Services;
+
+namespace Alcoholic.Areas.BackCenter.Controllers
+{
+    [Area("BackCenter")]
+    public class StaffController : Controller
+    {
+        private readonly db_a8de26_projectContext db;
+        private readonly HashService hash;
+        public StaffController(db_a8de26_projectContext db, HashService hash)
+        {
+            this.db = db;
+            this.hash = hash;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public List<Employee> GetAllMember()
+        {
+            return db.Employees.ToList();
+        }
+
+        [HttpPost]
+        public IActionResult Login([FromBody] Employee emp)
+        {
+            Employee? employee = (from em in db.Employees
+                                  where emp.EmpAccount == em.EmpAccount
+                                  select em).SingleOrDefault();
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            if (hash.GetHash(emp.EmpPassword) == employee.EmpPassword)
+            {
+                List<Claim> claims = new()
+                {
+                    new Claim(ClaimTypes.Name,emp.EmpName),
+                    new Claim(ClaimTypes.Role,emp.Role),
+                };
+                ClaimsIdentity claimsIdentity = new(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+                HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+                return Ok();
+            }
+            return NotFound();
+        }
+        [Authorize(Roles = "moderater,leader,staff")]
+        public IActionResult LogOut()
+        {
+            HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+            return Ok();
+        }
+        [Authorize(Roles = "moderater")]
+        [HttpPost]
+        public IActionResult Register([FromBody] Employee emp)
+        {
+            if (emp == null)
+            {
+                emp.Salt = Guid.NewGuid().ToString("N");
+                emp.EmpPassword = hash.GetHash((string)emp.EmpPassword.Concat(emp.Salt));
+                db.Add(emp);
+                db.SaveChanges();
+                return Ok();
+            }
+            return NotFound();
+        }
+
+    }
+}
