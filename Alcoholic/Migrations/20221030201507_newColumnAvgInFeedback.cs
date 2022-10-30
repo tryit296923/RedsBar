4f9f28d4b7ee14f454b93b524d81bd0bae89e54f
tryit296923 <tryit296923@gmail.com>
2022-10-31 06:23:09 +0800

    1. add new column in feedback 2. complete mainpage status

---
+++ Alcoholic/Migrations/20221030201507_newColumnAvgInFeedback.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Alcoholic.Migrations
+{
+    public partial class newColumnAvgInFeedback : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Join",
+                table: "Employee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 10, 31, 4, 15, 6, 908, DateTimeKind.Local).AddTicks(8984),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 10, 27, 19, 40, 56, 562, DateTimeKind.Local).AddTicks(1954));
+
+            migrationBuilder.AddColumn<int>(
+                name: "Average",
+                table: "Feedback",
+                type: "int",
+                nullable: true,
+                computedColumnSql: "(([Frequency]+[Environment]+[Serve]+[Dish]+[Price]+[Overall])/6)",
+                stored: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Average",
+                table: "Feedback");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Join",
+                table: "Employee",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2022, 10, 27, 19, 40, 56, 562, DateTimeKind.Local).AddTicks(1954),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2022, 10, 31, 4, 15, 6, 908, DateTimeKind.Local).AddTicks(8984));
+        }
+    }
+}
---
+++ Alcoholic/Models/DTO/MainPageModel.cs
@@ -0,0 +1,25 @@
+﻿namespace Alcoholic.Models.DTO
+{
+    public class MainPageModel
+    {
+        public int Total { get; set; }
+        public int GuestNum { get; set; }
+        public int MemberNum { get; set; }
+        public int Rate { get; set; }
+        public List<HotSales> HotSales { get; set; }
+    }
+    public class HotSales
+    {
+        public string ProductName { get; set; }
+        public string ImgPath { get; set; }
+        public int Quantity { get; set; }
+    }
+    public class SelectModel
+    {
+        public int STotal { get; set; }
+        public int SGuestNum { get; set; }
+        public int SMemberNum { get; set; }
+        public int SRate { get; set; }
+    }
+
+}
