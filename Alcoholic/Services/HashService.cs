dace18e47f3ce49f10a51cd2c76da40d5e20b1ee
tryit296923 <tryit296923@gmail.com>
2022-11-13 18:00:28 +0800

    change password encription

--- Alcoholic/Services/HashService.cs
+++ Alcoholic/Services/HashService.cs
@@ -9,12 +9,7 @@ namespace Alcoholic.Services
         {
             SHA256 sHA256 = SHA256.Create();
             byte[] bytes = sHA256.ComputeHash(Encoding.UTF8.GetBytes(input));
-            StringBuilder builder = new StringBuilder();
-            for (int i = 0; i < bytes.Length; i++)
-            {
-                builder.Append(bytes[i].ToString("X"));
-            }
-            return builder.ToString().ToLower();
+            return BitConverter.ToString(bytes, 0, bytes.Length)?.Replace("-", "")?.ToLower(); ;
         }
         public string GetHashHex(string input)
         {
